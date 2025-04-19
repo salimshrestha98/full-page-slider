d9d78f511ed991eaa2419bc4f7c1823f8e41b702
Salim Shrestha <salimshrestha98@gmail.com>
2025-04-19 20:18:46 +0545

    Add - Settings

--- src/full-page-content-slider/edit.js
+++ src/full-page-content-slider/edit.js
@@ -1,12 +1,27 @@
 import './style.scss';
 import './editor.scss';
 
-import { A11y, Navigation, Pagination, Scrollbar } from 'swiper/modules';
-import { useBlockProps, useInnerBlocksProps } from '@wordpress/block-editor';
+import {
+	EffectCards,
+	EffectCoverflow,
+	EffectCreative,
+	EffectCube,
+	EffectFlip,
+	Navigation,
+	Pagination,
+	Scrollbar
+} from 'swiper/modules';
+import { InspectorControls, useBlockProps, useInnerBlocksProps } from '@wordpress/block-editor';
+import {
+	PanelBody,
+	RangeControl,
+	SelectControl,
+	ToggleControl,
+} from '@wordpress/components';
+import { useEffect, useRef } from '@wordpress/element';
 
-import { Swiper } from 'swiper/react';
+import { Swiper } from 'swiper';
 import { __ } from '@wordpress/i18n';
-import { useEffect } from '@wordpress/element';
 import { useThemeColorResolver } from '../hooks/useThemeColorResolver';
 
 export default function Edit ( { attributes, setAttributes } ) {
@@ -14,7 +29,15 @@ export default function Edit ( { attributes, setAttributes } ) {
 	const {
 		backgroundColor,
 		textColor,
-		colors
+		colors,
+		previewMode,
+		direction,
+		effect,
+		speed,
+		navigation,
+		pagination,
+		loop,
+		scrollbar
 	} = attributes;
 
 
@@ -27,37 +50,27 @@ export default function Edit ( { attributes, setAttributes } ) {
 
 	const blockProps = useBlockProps();
 	const innerBlocksProps = useInnerBlocksProps(
-		{},
+		{ className: 'swiper-wrapper' },
 		{
-			renderAppender: false,
 			allowedBlocks: [ 'super-blocks/slide' ],
 			template: TEMPLATE,
 			templateLock: false
 		}
 	);
 
-	useEffect( () => {
-		document.querySelectorAll( '.swiper' ).forEach( ( swiper ) => {
-			const wrapper = swiper.querySelector( '.swiper-wrapper' );
-
-			if ( !wrapper ) return;
-
-			const slides = swiper.querySelectorAll( ':scope > .swiper-slide' );
-
-			slides.forEach( ( slide ) => {
-				if ( slide.parentElement !== wrapper )
-				{
-					wrapper.appendChild( slide );
-				}
-			} );
-		} );
-	}, [] );
 
 
+	/**
+	 * Convert theme palette color into proper hex format to use later in frontend.
+	 */
 	const getColorFromSlug = useThemeColorResolver();
 
 	useEffect( () => {
-		if ( backgroundColor && backgroundColor.startsWith( 'accent' ) && ( getColorFromSlug( backgroundColor ) != colors?.backgroundColor ) )
+		if (
+			backgroundColor &&
+			backgroundColor.startsWith( 'accent' ) &&
+			( getColorFromSlug( backgroundColor ) != colors?.backgroundColor )
+		)
 		{
 			setAttributes( {
 				colors:
@@ -68,7 +81,11 @@ export default function Edit ( { attributes, setAttributes } ) {
 			} );
 		}
 
-		if ( textColor && textColor.startsWith( 'accent' ) && ( getColorFromSlug( textColor ) != colors?.textColor ) )
+		if (
+			textColor &&
+			textColor.startsWith( 'accent' ) &&
+			( getColorFromSlug( textColor ) != colors?.textColor )
+		)
 		{
 			setAttributes( {
 				colors: {
@@ -79,27 +96,164 @@ export default function Edit ( { attributes, setAttributes } ) {
 		}
 	}, [ backgroundColor, textColor ] );
 
+
+
+	/**
+	 * Handle Swiper initialization and loading.
+	 */
+
+	const swiperRef = useRef( null );
+	const containerRef = useRef( null );
+
+	useEffect( () => {
+		let swiperModules = [];
+
+		if ( navigation ) swiperModules.push( Navigation );
+		if ( pagination ) swiperModules.push( Pagination );
+		if ( scrollbar ) swiperModules.push( Scrollbar );
+		if ( 'cube' === effect ) swiperModules.push( EffectCube );
+		if ( 'coverflow' === effect ) swiperModules.push( EffectCoverflow );
+		if ( 'flip' === effect ) swiperModules.push( EffectFlip );
+		if ( 'creative' === effect ) swiperModules.push( EffectCreative );
+		if ( 'cards' === effect ) swiperModules.push( EffectCards );
+
+		if ( swiperRef.current )
+		{
+			swiperRef.current.destroy( true, true );
+		}
+
+		const swiperArgs = {
+			modules: swiperModules,
+			direction: direction,
+			allowTouchMove: false,
+			simulateTouch: false,
+			touchStartPreventDefault: false,
+			touchStartForcePreventDefault: false,
+			initialSlide: 0,
+			centeredSlides: true,
+			spaceBetween: 50,
+			slidesPerView: "auto",
+			direction: direction || 'horizontal',
+			loop: !!loop,
+			slidesPerView: 1,
+			effect: effect || 'slide',
+			speed: speed,
+			navigation: {
+				nextEl: '.swiper-button-next',
+				prevEl: '.swiper-button-prev'
+			},
+			pagination: {
+				el: '.swiper-pagination',
+				clickable: true
+			},
+			scrollbar: {
+				el: '.swiper-scrollbar',
+				draggable: true
+			}
+		};
+
+		swiperRef.current = new Swiper( containerRef.current, swiperArgs );
+
+	}, [ direction, loop, pagination, navigation, scrollbar, effect, speed ] );
+
+
 	return (
-		<div { ...blockProps } className={ `${ blockProps.className } full-page-slider` }>
-			<Swiper
-				modules={ [ Navigation, Pagination, Scrollbar, A11y ] }
-				direction='horizontal'
-				allowTouchMove={ true }
-				simulateTouch={ true }
-				touchStartPreventDefault={ false }
-				touchStartForcePreventDefault={ false }
-				initialSlide={ 0 }
-				centeredSlides
-				spaceBetween={ 50 }
-				slidesPerView="auto"
-				navigation
-				pagination={ { clickable: true } }
-				scrollbar={ { draggable: true } }
-				onSwiper={ ( swiper ) => console.log( swiper ) }
-				onSlideChange={ () => console.log( 'slide change' ) }
-				{ ...innerBlocksProps }
-			>
-			</Swiper>
-		</div>
+		<>
+			<InspectorControls>
+				<PanelBody title={ __( "General", 'super-blocks' ) }>
+					<SelectControl
+						label="Preview Mode"
+						value={ previewMode }
+						options={ [
+							{ label: 'Smartphone', value: 'smartphone' },
+							{ label: 'Tablet', value: 'tablet' },
+							{ label: 'Desktop', value: 'desktop' }
+						] }
+						onChange={ ( value ) => setAttributes( { previewMode: value } ) }
+					/>
+				</PanelBody>
+
+				<PanelBody title="Slider" initialOpen={ false }>
+					<SelectControl
+						label="Direction"
+						value={ direction }
+						options={ [
+							{ label: 'Horizontal', value: 'horizontal' },
+							{ label: 'Vertical', value: 'vertical' },
+						] }
+						onChange={ ( value ) => setAttributes( { direction: value } ) }
+					/>
+
+					<SelectControl
+						label="Slide Effect"
+						value={ effect }
+						options={ [
+							{ label: 'Slide', value: 'slide' },
+							{ label: 'Cube', value: 'cube' },
+							{ label: 'Coverflow', value: 'coverflow' },
+							{ label: 'Flip', value: 'flip' },
+							{ label: 'Cards', value: 'cards' },
+						] }
+						onChange={ ( value ) => setAttributes( { effect: value } ) }
+					/>
+
+					<RangeControl
+						label="Slide Speed (ms)"
+						value={ speed }
+						onChange={ ( newSpeed ) => setAttributes( { speed: newSpeed } ) }
+						min={ 100 }
+						max={ 1000 }
+						step={ 100 }
+					/>
+
+					<label className="sb-setting-label">{ __( "Enable Prev/Next Buttons", 'super-blocks' ) }</label>
+					<ToggleControl
+						checked={ navigation }
+						onChange={ ( value ) => setAttributes( { navigation: value } ) }
+					/>
+
+					<label className="sb-setting-label">{ __( "Enable Pagination", 'super-blocks' ) }</label>
+					<ToggleControl
+						checked={ pagination }
+						onChange={ ( value ) => setAttributes( { pagination: value } ) }
+					/>
+
+					<label className="sb-setting-label">{ __( "Enable Scrollbar", 'super-blocks' ) }</label>
+					<ToggleControl
+						checked={ scrollbar }
+						onChange={ ( value ) => setAttributes( { scrollbar: value } ) }
+					/>
+
+					<label className="sb-setting-label">{ __( "Loop Slides", 'super-blocks' ) }</label>
+					<ToggleControl
+						checked={ loop }
+						onChange={ ( value ) => setAttributes( { loop: value } ) }
+					/>
+
+				</PanelBody>
+			</InspectorControls>
+
+			<div { ...blockProps } className={ `${ blockProps.className } full-page-slider` }>
+				<div className="swiper" ref={ containerRef }>
+					<div { ...innerBlocksProps }>
+					</div>
+
+					{ navigation && (
+						<>
+							<div className="swiper-button-next"></div>
+							<div className="swiper-button-prev"></div>
+						</>
+					) }
+
+					{ pagination && (
+						<div className="swiper-pagination"></div>
+					) }
+
+					{ scrollbar && (
+						<div className="swiper-scrollbar"></div>
+					) }
+				</div>
+			</div>
+		</>
 	);
 }

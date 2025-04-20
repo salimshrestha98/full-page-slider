0984b23c93690e6e27f6e378b2df867b8bc22521
Salim Shrestha <salimshrestha98@gmail.com>
2025-04-20 07:28:46 +0545

    Fix - Configuration Issues
    Fix - Commit uncomitted index.php file
    Fix - Set proper required version
    Fix - Remove necessary files from .gitignore

--- includes/Blocks/Blocks.php
+++ includes/Blocks/Blocks.php
@@ -21,16 +21,11 @@ class Blocks {
 	 * Register the blocks in server side.
 	 */
 	public function register_blocks() {
-		if ( function_exists( 'wp_register_block_types_from_metadata_collection' ) ) {
-			wp_register_block_types_from_metadata_collection( SB_PLUGIN_DIR . '/build', SB_PLUGIN_DIR . '/build/blocks-manifest.php' );
-			return;
-		}
-
 		if ( function_exists( 'wp_register_block_metadata_collection' ) ) {
 			wp_register_block_metadata_collection( SB_PLUGIN_DIR . '/build', SB_PLUGIN_DIR . '/build/blocks-manifest.php' );
 		}
 
-		$manifest_data = require SB_PLUGIN_DIR . '/build/blocks-manifest.php';
+		$manifest_data = include SB_PLUGIN_DIR . '/build/blocks-manifest.php';
 		foreach ( array_keys( $manifest_data ) as $block_type ) {
 			register_block_type( SB_PLUGIN_DIR . "/build/{$block_type}" );
 		}
---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+ * Plugin Name:       Super Blocks
+ * Description:       Super Blocks Plugin.
+ * Version:           1.0.0
+ * Requires at least: 6.2
+ * Requires PHP:      7.4
+ * Author:            Salim Shrestha
+ * Author URI:        https://github.com/salimshrestha98/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       super-blocks
+ * Domain Path:       /languages
+ */
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+use SuperBlocks\Plugin;
+
+require_once 'vendor/autoload.php';
+
+/**
+ * Define plugin constants.
+ */
+
+
+define( 'SB_VERSION', '1.0.0' );
+define( 'SB_PLUGIN_FILE', __FILE__ );
+define( 'SB_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+define( 'SB_PLUGIN_URL', plugins_url( '', __FILE__ ) );
+define( 'SB_PLUGIN_BASENAME', plugin_basename( __FILE__ ) );
+define( 'SB_TEXT_DOMAIN', 'super-blocks' );
+
+Plugin::instance()->init();
